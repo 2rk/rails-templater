32794e772be4bca525c155e1f990dbc953399a22
Tom Ridge <tom.ridge@bigpond.com>
2013-10-18 08:55:01 +1000

    use replacement scaffold controller

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,15 +8,13 @@ create_file 'README'
 create_file 'log/.gitkeep'
 create_file 'tmp/.gitkeep'
 
-gsub_file 'config/application.rb', 'require "rails/test_unit/railtie"', '# require "rails/test_unit/railtie"'
-
-get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/html5.js"
-
 git :init
 
 append_file '.gitignore', load_template('gitignore','git')
 
-gem 'rails3-generators'
 
 #haml stuff has moved out of rails3-generators
 gem 'haml-rails'
+
+# load new scaffold controller
+create_file 'lib/templates/rails/scaffold_controller/controller.rb', load_template('scaffold_controller/controller.rb','rails')
